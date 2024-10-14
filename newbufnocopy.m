0ea6bbbf7222e6929db7d2163c9166f2eab0ac85
sureshe456 <160699174+sureshe456@users.noreply.github.com>
2024-10-14 11:33:08 +0530

    Updated newbufnocopy.m

--- newbufnocopy.m
+++ newbufnocopy.m
@@ -9,29 +9,24 @@
 int main(int argc, char** argv)
 {
     id<MTLDevice> device = MTLCreateSystemDefaultDevice();
-    int *buffer = malloc(4 * sizeof(int));
-    buffer[0] = 10;
-    buffer[1] = 20;
-    buffer[2] = 30;
-    buffer[3] = 40;
-    for (int i = 0; i < 4; i++) {
-        printf("Malloc data[%d] = %d\n", i, buffer[i]);
-    }
 
-    NSMutableData *ns_mutable = [NSMutableData dataWithBytesNoCopy:buffer length:4 * sizeof(int)];
-    int length = [ns_mutable length];
-    int* ns_mutable_ptr = [ns_mutable mutableBytes];
-    printf("NSMutable data created\n");
-    printf("Length: %d\n", length);
-    for (int i = 0; i < 4; i++) {
-        printf("NSMutable data[%d] = %d\n", i, ns_mutable_ptr[i]);
-    }
+    int length = 16;
 
-    id<MTLBuffer> device_buffer = [device newBufferWithBytesNoCopy:ns_mutable_ptr
-                                    length:length
-                                    options:0
-                                    deallocator:nil
-                                    ];
+
+    void *updateAddress;
+    kern_return_t err = vm_allocate((vm_map_t)mach_task_self(),
+                                            (vm_address_t*)&updateAddress,
+                                            length,
+                                            VM_FLAGS_ANYWHERE);
+    for (int i = 0; i < 4; i++) {
+        ((int*)updateAddress)[i] = i;
+    }
+    assert(err == KERN_SUCCESS);
+    id<MTLBuffer> device_buffer = [device newBufferWithBytesNoCopy:updateAddress
+                                                            length:length
+                                                           options:0
+                                                       deallocator:nil
+    ];
     printf("Device buffer created\n");
     int device_buffer_length = [device_buffer length];
     printf("Length: %d\n", device_buffer_length);
