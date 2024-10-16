00eb558ac8c53f508b961bfbc02361a0f27a4b4d
sureshe456 <160699174+sureshe456@users.noreply.github.com>
2024-10-16 19:52:59 +0530

    Update newbufnocopy.m

--- newbufnocopy.m
+++ newbufnocopy.m
@@ -10,11 +10,6 @@
 int main(int argc, char** argv)
 {
     id<MTLDevice> device = MTLCreateSystemDefaultDevice();
-    if (!device) {
-        printf("Metal is not supported on this device\n");
-        return -1;
-    }
-
     int length = 16;
     void *updateAddress;
 
@@ -22,34 +17,21 @@ int main(int argc, char** argv)
     int alignment = 16;  // Metal usually requires at least 16-byte alignment
     int result = posix_memalign(&updateAddress, alignment, length);
     
-    if (result != 0) {
-        printf("Memory alignment failed with error: %d\n", result);
-        return -1;
-    }
-
-    // Initialize the buffer with some test data
     for (int i = 0; i < 4; i++) {
         ((int*)updateAddress)[i] = i;
     }
 
-    // Create a Metal buffer with newBufferWithBytes (this will copy the data)
+    // Create a Metal buffer with newBufferWithBytes
     id<MTLBuffer> device_buffer = [device newBufferWithBytes:updateAddress
                                                       length:length
-                                                     options:0];
+                                                     options:0
+                                                     deallocator:nil
+    ];
     
-    if (!device_buffer) {
-        printf("Failed to create Metal buffer\n");
-        free(updateAddress);
-        return -1;
-    }
-
     printf("Device buffer created\n");
-
-    // Check the buffer length
     int device_buffer_length = (int)[device_buffer length];
     printf("Length: %d\n", device_buffer_length);
     
-    // Check the contents of the buffer
     int *data = (int*)[device_buffer contents];
     printf("Data pointer obtained\n");
 
@@ -59,13 +41,10 @@ int main(int argc, char** argv)
         return 0;
     }
 
-    // Print the contents of the buffer
     for (int i = 0; i < 4; i++) {
         printf("Device buffer[%d] = %d\n", i, data[i]);
     }
-
-    // Free the aligned memory after usage
+    
     free(updateAddress);
-
     return 0;
 }
